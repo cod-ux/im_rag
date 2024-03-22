f7abfbf0f3bfc25659e479af614127f98bdae2f8
Surya Govindasamy Ganesan <suryaganesan925@gmail.com>
2024-03-22 15:17:22 +0000

    sdjfn

--- im_rag.py
+++ im_rag.py
@@ -11,8 +11,6 @@ github_path = "/embeddings/"
 
 db = FAISS.load_local(github_path+'faiss_index', embeddings, allow_dangerous_deserialization=True)
 
-db = FAISS.load_local('faiss_index', embeddings, allow_dangerous_deserialization=True)
-
 api_key = st.secrets["OPENAI_API_KEY"]
 
 
